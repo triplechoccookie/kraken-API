0f9bcd8824c03e2c919c3ece24450ba38f4ae09b
Karsten Heuschkel <karsten.heuschkel@web.de>
2014-12-15 23:25:35 +0100

    some fixes

--- run.py
+++ run.py
@@ -10,13 +10,24 @@ print('Serverzeit ist ' + time.strftime('%a, %d %b %Y %H:%M:%S +0000', krakenAPI
 
 print(krakenAPI.get_assets())
 
-lasttimestamp = '0'
+lasttimestampXBT = '0'
+lasttimestampLTC = '0'
 while True:
-    data = krakenAPI.get_ohcl_data('XLTCZEUR', '1', lasttimestamp)
-    for item in data:
-        print(item)
-    lasttimestamp = data[-1]['timestamp']
-    time.sleep(10)
+    data = krakenAPI.get_ohcl_data('XLTCZEUR', '1', lasttimestampLTC)
+    if lasttimestampLTC != data[-1]['timestamp']:
+        lasttimestampLTC = data[-1]['timestamp']
+        print('EUR/LTC')
+        for item in data:
+            print(item)
+
+    data = krakenAPI.get_ohcl_data('XXBTZEUR', '1', lasttimestampXBT)
+    if lasttimestampXBT != data[-1]['timestamp']:
+        lasttimestampXBT = data[-1]['timestamp']
+        print('EUR/XBT')
+        for item in data:
+            print(item)
+
+    time.sleep(5)
 
 
 print('closing')
