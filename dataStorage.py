0a1013369e2b25516100f7b5e8db3d7fd85ebdb3
Karsten Heuschkel <karsten.heuschkel@web.de>
2014-12-16 15:15:12 +0100

    database implementation

--- dataStorage.py
+++ dataStorage.py
@@ -2,17 +2,35 @@ __author__ = 'Karsten'
 
 import sqlite3
 
-connection = sqlite3.connect('database.db')
-c = connection.cursor()
 
 def init_storage():
+    connection = sqlite3.connect('database.db')
+    c = connection.cursor()
     c.execute('''CREATE TABLE ohcl_1m (timestamp INTEGER PRIMARY KEY, high INTEGER, low INTEGER, open INTEGER,
               close INTEGER, volume INTEGER, count INTEGER)''')
     connection.commit()
 
+    connection.close()
+
+
 def get_last_data_timestamp():
-    c.execute('SELECT * FROM ohcl_1m')
-    print(c.fetchall())
+    connection = sqlite3.connect('database.db')
+    c = connection.cursor()
+    c.execute('SELECT MAX(timestamp) FROM ohcl_1m')
+    data = c.fetchall()
+    connection.close()
+
+    return data[0][0]
 
 
-# def store_new_data():
+def store_new_data(timestamp, high, low, open, close, volume, count):
+    connection = sqlite3.connect('database.db')
+    c = connection.cursor()
+
+    data = (timestamp, high, low, open, close, volume, count)
+
+    c.execute('REPLACE INTO ohcl_1m VALUES (?,?,?,?,?,?,?)', data)
+
+    connection.commit()
+
+    connection.close()
