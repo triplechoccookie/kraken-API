0759fbbb7e5171bc77e0e5da4107c4ad1fb633e7
triplechoccookie <karsten.heuschkel@web.de>
2015-02-18 17:42:01 +0100

    fixed simulation print out

--- test.py
+++ test.py
@@ -4,9 +4,10 @@ import analysis
 
 
 for i in range(1, 100):
-    print(i)
+
     for j in range(1, 100):
         if i > j:
+            print(str(i) + '/' + str(j))
             result = analysis.simulateTradeOnMaCrossoverWithFees(i, j, 0.002, 100)
             if result['money'] >= 100:
                 print(str(i) + '/' + str(j) + ': ' + str(result))
