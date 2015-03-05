c84a4fe73bac814a98541f26bb713cf04042079e
triplechoccookie <karsten.heuschkel@web.de>
2015-03-05 15:21:18 +0100

    some code cleaning

--- krakenAPI.py
+++ krakenAPI.py
@@ -5,7 +5,7 @@ import time
 
 
 def get_server_time():
-    data = restHelper.get_data_from_URL('https://api.kraken.com/0/public/Time')
+    data = restHelper.get_data_from_url('https://api.kraken.com/0/public/Time')
     if data['error'] is []:
         data = time.gmtime(data['result']['unixtime'])
     else:
@@ -15,7 +15,7 @@ def get_server_time():
 
 
 def get_assets():
-    data = restHelper.get_data_from_URL('https://api.kraken.com/0/public/Assets')
+    data = restHelper.get_data_from_url('https://api.kraken.com/0/public/Assets')
     if data['error'] is []:
         data = data['result']
     else:
@@ -25,7 +25,7 @@ def get_assets():
 
 
 def get_ticker(pair='XBTEUR'):
-    data = restHelper.get_data_from_URL('https://api.kraken.com/0/public/Ticker', {'pair': pair})
+    data = restHelper.get_data_from_url('https://api.kraken.com/0/public/Ticker', {'pair': pair})
     if data['error'] is []:
         data = data['result']
     else:
@@ -34,7 +34,7 @@ def get_ticker(pair='XBTEUR'):
     return data
 
 def get_ohcl_data(pair='XBTEUR', interval='15', since='0'):
-    data = restHelper.get_data_from_URL('https://api.kraken.com/0/public/OHLC', {'pair': pair, 'interval': interval,
+    data = restHelper.get_data_from_url('https://api.kraken.com/0/public/OHLC', {'pair': pair, 'interval': interval,
                                                                                  'since': since})
     if data['error'] is []:
         data = data['result'][pair]
